21554ee5932c5791a780445e8ad2bfd5b234d51f
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2022-05-26 21:15:24 +0600

    sppinner style

--- src/components/Templates/Loading.js
+++ src/components/Templates/Loading.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const Loading = () => {
     return (
         <div>
-            <div className="text-center">Loading . . . </div>
+            <div className="py-5 d-flex justify-content-center align-items-center">
+                <Spinner animation="grow" /><Spinner animation="grow" /><Spinner animation="grow" /><Spinner animation="grow" /><Spinner animation="grow" />
+            </div>
         </div>
     );
 };
